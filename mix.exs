ffd053603276724f5d0dff3a29f7ba05e0a90f6a
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2026-01-12 13:32:36 -0600

    Add dialyxir depenency

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule BibleScrapper.MixProject do
     [
       {:req, "~> 0.5.16"},
       {:floki, "~> 0.38.0"},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: :dev, runtime: false},
       {:ex_doc, "~> 0.39.1", only: :dev, runtime: false}
     ]
